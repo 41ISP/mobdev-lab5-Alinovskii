555078f84b61f1dfe915b1958b6aa521b4d50450
MaksimAli <132120990+MaksimAli@users.noreply.github.com>
2025-02-04 08:04:42 +0000

    Mobile

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,25 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import 'react-native-reanimated';
-
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { StyleSheet,  Text,  TouchableOpacity, View } from 'react-native';
+
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+  const [counter, setCounter] = useState(0);
+
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
+  const handleclick = () => {
+    setCounter((value) => value + 1);
+  }
 
   useEffect(() => {
     if (loaded) {
@@ -28,12 +32,28 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleclick}>
+          <Text style={{padding: 50}}>-</Text>
+        </TouchableOpacity>
+        <Text style={styles.text}>
+          {counter}
+        </Text>
+      </View>
     </ThemeProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 100,
+    flexDirection: "row",
+  },
+  text: {
+    fontSize: 30,
+    color: "#ff0000",
+  }
+
+});
